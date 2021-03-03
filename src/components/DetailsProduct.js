8945305ba0845971c6b4200df32166570301d765
bruteforce91 <corsarosalvatore91@yahoo.it>
2021-03-03 11:03:49 +0100

    fix bug modalView

--- src/components/DetailsProduct.js
+++ src/components/DetailsProduct.js
@@ -4,19 +4,13 @@ import React,{useState} from 'react';
 
 export default function DetailsProduct(props) {
   const { title, image, price, description} = props.data;
-  const [isClose,setIsClose]=useState(false)
-  
- 
-  function handleCloseModal(){
-    setIsClose(true);
-  }
 
   return (
-    !isClose?
+    props.isOpen ?
     <div className="modal">
-      <div className="modal__overlay">
+      <div className="modal__overlay"  onClick={()=>props.isOpen()} >
         <div className="modal__body">
-            <button type="button" onClick={handleCloseModal} className="btnCloseModal">X</button>
+            <button type="button" onClick={()=>props.isOpen()} className="btnCloseModal">X</button>
             <h2>{title}</h2>
             <img src={image} />
             <span>€ {price.toFixed(2)}</span>
