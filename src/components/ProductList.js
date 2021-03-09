543575593ca4742f905336a12c7378b41257ef08
Marco Ziliani <marco.ziliani@musement.com>
2021-03-10 00:07:54 +0100

    refactor: use generic sidebar modal for cart

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,7 +6,7 @@ import Search from "./Search";
 import "./ProductList.css";
 import CategoriesFilter from "./CategoriesFilter";
 
-function ProductList({ products, categories, openProductModal }) {
+function ProductList({ products, categories, openModalProduct }) {
   const [searchTerm, setSearchTerm] = useState();
   const [selectedCategories, setSelectedCategories] = useState([]);
 
@@ -33,7 +33,7 @@ function ProductList({ products, categories, openProductModal }) {
           <Product
             product={product}
             key={product.id}
-            openProductModal={() => openProductModal(product)}
+            openModalProduct={() => openModalProduct(product)}
           />
         ))}
       </div>
@@ -44,7 +44,7 @@ function ProductList({ products, categories, openProductModal }) {
 ProductList.propTypes = {
   products: PropTypes.array.isRequired,
   categories: PropTypes.array.isRequired,
-  openProductModal: PropTypes.func.isRequired,
+  openModalProduct: PropTypes.func.isRequired,
 };
 
 export default ProductList;
