25a4246aa24409371bc2cb699b40f5c003135be6
Marco Ziliani <marco.ziliani@musement.com>
2021-03-10 00:52:35 +0100

    fix: format total price in header

--- src/components/HeaderCart.js
+++ src/components/HeaderCart.js
@@ -1,11 +1,12 @@
 import PropTypes from "prop-types";
 
+import { formatPrice } from "../services/utils";
 import "./HeaderCart.css";
 
-function HeaderCart({ cartTotal, cartSize, products, onCartClick }) {
+function HeaderCart({ cartTotal, cartSize, onCartClick }) {
   return (
     <div className="HeaderCart">
-      {!!cartSize && <span className="price">{cartTotal}</span>}
+      {!!cartSize && <span className="price">{formatPrice(cartTotal)}</span>}
       <span className="icon" onClick={onCartClick}>
         <i className="fas fa-shopping-cart"></i>
         {!!cartSize && <span className="qty">{cartSize}</span>}
