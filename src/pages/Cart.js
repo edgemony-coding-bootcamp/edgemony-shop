a23e1b2edca8d5669cffec7d17302d465ad94f45
bruteforce91 <corsarosalvatore91@yahoo.it>
2021-03-20 23:14:16 +0100

    fetch patch cart

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import CartProduct from "./../components/CartProduct"
 import "./../components/CartModal.css";
+import { Link } from "react-router-dom";
+import "./Cart.css"
 
 import { formatPrice } from "../services/utility";
 
 function Cart({products, removeFromCart, setProductQuantity, totalPrice}) {
     return (
-        <div>
+        <div className="Cart">
         {products.map((product) => {
           console.log("product", product);
           return (
@@ -16,6 +18,11 @@ function Cart({products, removeFromCart, setProductQuantity, totalPrice}) {
           );
         })}
         <span>Total: {formatPrice(totalPrice.toFixed(2))}</span>
+        <button>
+          <Link to="/checkout">
+            Go to Checkout
+          </Link>
+        </button>
       </div>
     )
 }
